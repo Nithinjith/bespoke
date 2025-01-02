1f20a95a5ac34351f7aa4c961e2ad1807a767e4b
nithinjith <nithinjith.p@gmail.com>
2025-01-02 07:18:29 +0530

    Connected with Firebase

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,18 +1,18 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-//
-// part 'user_model.freezed.dart';
-// part 'user_model.g.dart';
-//
-// @freezed
-// class UserModel with _$UserModel {
-//   const factory UserModel({
-//     required int id,
-//     required String name,
-//     required String email,
-//     @Default(false) bool isActive,
-//     DateTime? createdAt,
-//   }) = _UserModel;
-//
-//   /// JSON serialization methods
-//   factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
-// }
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required int id,
+    required String name,
+    required String email,
+    @Default(false) bool isActive,
+    DateTime? createdAt,
+  }) = _UserModel;
+
+  /// JSON serialization methods
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+}
---
+++ lib/models/user_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      id: (json['id'] as num).toInt(),
+      name: json['name'] as String,
+      email: json['email'] as String,
+      isActive: json['isActive'] as bool? ?? false,
+      createdAt: json['createdAt'] == null
+          ? null
+          : DateTime.parse(json['createdAt'] as String),
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'isActive': instance.isActive,
+      'createdAt': instance.createdAt?.toIso8601String(),
+    };
